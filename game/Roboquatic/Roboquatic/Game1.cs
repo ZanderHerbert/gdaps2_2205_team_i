6f4767ff2d1c9185d0f8f47119316a10e65ad41d
ZanderHerbert <70411825+ZanderHerbert@users.noreply.github.com>
2021-02-15 17:43:40 -0500

    Created the monogame project, made a basic player class, and made it so that the player could move around the screen.

---
+++ game/Roboquatic/Roboquatic/Game1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Roboquatic
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Player player;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            player = new Player(1, new Rectangle(0, 0, 5, 5));
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            player.Sprite = Content.Load<Texture2D>("PlayerPlaceholder");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                player.MoveDown();
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                player.MoveUp();
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                player.MoveLeft();
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                player.MoveRight();
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(player.Sprite, player.Position, Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ game/Roboquatic/Roboquatic/Player.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Roboquatic
+{
+    class Player
+    {
+        private int speed;
+        private Rectangle position;
+        private Texture2D sprite;
+
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+        public Texture2D Sprite
+        {
+            get { return sprite; }
+            set { sprite = value; }
+        }
+        public int PositionX
+        {
+            get { return position.X; }
+            set { position.X = value; }
+        }
+        public int PositionY
+        {
+            get { return position.Y; }
+            set { position.Y = value; }
+        }
+        public Rectangle Position
+        {
+            get { return position; }
+        }
+
+        public Player(int speed, Rectangle position)
+        {
+            this.speed = speed;
+            this.position = position;
+        }
+
+        public void MoveUp()
+        {
+            position.Y = position.Y - speed * 2;
+        }
+        public void MoveDown()
+        {
+            position.Y = position.Y + speed * 2;
+        }
+        public void MoveLeft()
+        {
+            position.X = position.X - speed * 2;
+        }
+        public void MoveRight()
+        {
+            position.X = position.X + speed * 2;
+        }
+    }
+}
