d087eecbe487c0c07a1006d6ad0a725208db5a97
ZanderHerbert <70411825+ZanderHerbert@users.noreply.github.com>
2021-04-01 18:47:20 -0400

    Made a better basis for the boss class

--- game/Roboquatic/Roboquatic/Boss.cs
+++ game/Roboquatic/Roboquatic/Boss.cs
@@ -4,7 +4,39 @@ using System.Text;
 
 namespace Roboquatic
 {
-    class Boss
+    class Boss 
     {
+        public void Update(int timer, Random rng)
+        {
+            if(timer % 360 == 359)
+            {
+                switch(rng.Next(0, 3))
+                {
+                    case 0:
+                        ShotPatternOne();
+                        break;
+
+                    case 1:
+                        ShotPatternTwo();
+                        break;
+
+                    case 2:
+                        ShotPatternThree();
+                        break;
+                }
+            }
+        }
+        public void ShotPatternOne()
+        {
+
+        }
+        public void ShotPatternTwo()
+        {
+
+        }
+        public void ShotPatternThree()
+        {
+
+        }
     }
 }
